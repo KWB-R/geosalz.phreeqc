d1285402c7030c46ff14232bb8766999f174cf44
mrustl <michael.rustler@kompetenz-wasser.de>
2022-05-25 10:49:31 +0200

    Add read_simulations() (closes #3)
    in addition undo changes by @Dwightbaldwin as explained here: https://github.com/KWB-R/geosalz.phreeqc/commit/cec88482a66b5eaa4a30f21b674a00927efe4453

--- R/prepare_lab_data.R
+++ R/prepare_lab_data.R
@@ -19,7 +19,6 @@ add_solution_id <- function(samples)  {
 #'
 #' @param samples data.frame with samples in wide format, i.e. parameters are
 #' columns and each row is one sample
-#' Coerce samples data.frame to as.character to address potential input format error
 #' @return  data.frame with samples in tidy format, i.e. parameters are
 #' columns and each row is one value
 #' @export
@@ -32,7 +31,6 @@ tidy_samples <- function(samples) {
   col_names <- names(samples)[!names(samples) %in% c("solution_id", "solution", "units")]
 
   samples %>%
-    mutate_all(as.character) %>%
     add_solution_id() %>%
     tidyr::pivot_longer(cols = col_names,
                         names_to = "parameter",
